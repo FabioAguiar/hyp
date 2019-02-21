55d9c0d77a4c08515cf66f71d0d573f1e3ca923c
Fabio Aguiar <fabioaguiar.ti@gmail.com>
2019-02-21 16:38:27 -0300

    Plot de gráfico com informações de estação metereológica

--- hyp_app/static/js/weather/weather_station.js
+++ hyp_app/static/js/weather/weather_station.js
@@ -20,9 +20,160 @@ $(function(){
 		'velocidadeVento' : '',
 		'nascerSol' : '',
 		'porSol' : '',
-		'entrelinhar_condicao' : ''
+		'entrelinhar_condicao' : '',
+		'previsao' : '[]'
 	}
 
+
+
+    function plotarChart(previsao){
+
+
+		var randomScalingFactor = function() {
+			return Math.round(Math.random() * 100);
+		};
+
+		var config = {
+			type: 'line',
+			data: {
+				labels: [previsao[0].date, previsao[1].date, previsao[2].date, previsao[3].date, previsao[4].date, previsao[5].date, previsao[6].date],
+				datasets: [{
+					label: 'Temperatura máxima',
+					backgroundColor: window.chartColors.red,
+					borderColor: window.chartColors.red,
+					data: [
+						previsao[0].max,
+						previsao[1].max,
+						previsao[2].max,
+						previsao[3].max,
+						previsao[4].max,
+						previsao[5].max,
+						previsao[6].max
+					],
+					fill: false,
+				}, {
+					label: 'Temperatura mínima',
+					fill: false,
+					backgroundColor: window.chartColors.blue,
+					borderColor: window.chartColors.blue,
+					data: [
+						previsao[0].min,
+						previsao[1].min,
+						previsao[2].min,
+						previsao[3].min,
+						previsao[4].min,
+						previsao[5].min,
+						previsao[6].min
+					],
+				} ]
+			},
+			options: {
+				responsive: true,
+				title: {
+					display: true,
+					text: 'Previsão do tempo em Recife'
+				},
+				tooltips: {
+					mode: 'index',
+					intersect: false,
+				},
+				hover: {
+					mode: 'nearest',
+					intersect: true
+				},
+				scales: {
+					xAxes: [{
+						display: true,
+						scaleLabel: {
+							display: true,
+							labelString: 'Dias'
+						}
+					}],
+					yAxes: [{
+						display: true,
+						scaleLabel: {
+							display: true,
+							labelString: 'Temperatura'
+						},
+						ticks: {
+							min: 0,
+							max: 40,
+
+							// forces step size to be 5 units
+							stepSize: 5
+						}
+					}]
+				}
+			}
+		};
+
+		function carregarChart() {
+			var ctx = document.getElementById('canvas').getContext('2d');
+			window.myLine = new Chart(ctx, config);
+		};
+
+		carregarChart();
+
+		/*document.getElementById('randomizeData').addEventListener('click', function() {
+			config.data.datasets.forEach(function(dataset) {
+				dataset.data = dataset.data.map(function() {
+					return randomScalingFactor();
+				});
+			});
+
+			window.myLine.update();
+		});
+
+		var colorNames = Object.keys(window.chartColors);
+		document.getElementById('addDataset').addEventListener('click', function() {
+			var colorName = colorNames[config.data.datasets.length % colorNames.length];
+			var newColor = window.chartColors[colorName];
+			var newDataset = {
+				label: 'Dataset ' + config.data.datasets.length,
+				backgroundColor: newColor,
+				borderColor: newColor,
+				data: [],
+				fill: false
+			};
+
+			for (var index = 0; index < config.data.labels.length; ++index) {
+				newDataset.data.push(randomScalingFactor());
+			}
+
+			config.data.datasets.push(newDataset);
+			window.myLine.update();
+		});
+
+		document.getElementById('addData').addEventListener('click', function() {
+			if (config.data.datasets.length > 0) {
+				var month = MONTHS[config.data.labels.length % MONTHS.length];
+				config.data.labels.push(month);
+
+				config.data.datasets.forEach(function(dataset) {
+					dataset.data.push(randomScalingFactor());
+				});
+
+				window.myLine.update();
+			}
+		});
+
+		document.getElementById('removeDataset').addEventListener('click', function() {
+			config.data.datasets.splice(0, 1);
+			window.myLine.update();
+		});
+
+		document.getElementById('removeData').addEventListener('click', function() {
+			config.data.labels.splice(-1, 1); // remove the label first
+
+			config.data.datasets.forEach(function(dataset) {
+				dataset.data.pop();
+			});
+
+			window.myLine.update();
+		});
+		*/
+
+    }
 
 	function displayCallbackOWDData(data){
 
@@ -77,6 +228,10 @@ $(function(){
 		hgbr.nascerSol = data.results.sunrise;
 		hgbr.porSol = data.results.sunset;
 		hgbr.entrelinhar_condicao = data.results.condition_slug;
+		hgbr.previsao = data.results.forecast;
+
+
+		plotarChart(hgbr.previsao);
 
 
 	    $("#temp_hgbr").html(hgbr.temp);
@@ -104,5 +259,18 @@ $(function(){
 	}
 
 	getHGBrasilData(displayCallbackHGBrasilData);
+
+		/*
+        "date": "22/02",
+        "weekday": "Sex",
+        "max": "28",
+        "min": "25",
+        "description": "Tempestades",
+        "condition": "storm"
+        */
+
+
+
+
 
 });
