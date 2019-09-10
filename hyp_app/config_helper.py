9589a8d4db1c65d38a36eb8a7f9d4838ebeaebaa
Fabio Aguiar <fabioaguiar.ti@gmail.com>
2019-09-10 09:55:00 -0300

    config firebase

--- hyp_app/config_helper.py
+++ hyp_app/config_helper.py
@@ -2,12 +2,12 @@
 
 def configFirebase():
 	config = {
-		'apiKey': "AIzaSyBse-xWFqcWnnMSqvJsmt1MP7ILOAHw7jw",
-		'authDomain': "hypmobile-51803.firebaseapp.com",
-		'databaseURL': "https://hypmobile-51803.firebaseio.com",
-		'projectId': "hypmobile-51803",
-		'storageBucket': "hypmobile-51803.appspot.com",
-		'messagingSenderId': "617864492019",
-		'appId': "1:617864492019:web:a95457dab1bfacf7a19886"
+		'apiKey': "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+		'authDomain': "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+		'databaseURL': "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+		'projectId': "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+		'storageBucket': "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+		'messagingSenderId': "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+		'appId': "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 	}
 	return config
